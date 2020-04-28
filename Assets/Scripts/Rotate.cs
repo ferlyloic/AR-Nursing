4c17fa2f37b749424f14fafd83e5fa2be92af045
ferlyloic <ferlyloic@yahoo.fr>
2020-04-28 11:01:57 +0200

    Rotation added to brain

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float r_x = 0f;
+    public float r_y = 5f;
+    public float r_z = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(r_x, r_y, r_z);
+    }
+}
