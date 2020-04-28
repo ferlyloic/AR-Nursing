4e7c2ec2206dba295f38636b683db337a60c789e
ferlyloic <ferlyloic@yahoo.fr>
2020-04-28 18:59:00 +0200

    Update Rotate.cs
    slowdown from 5 to 1

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
     public float r_x = 0f;
-    public float r_y = 5f;
+    public float r_y = 1f;
     public float r_z = 0f;
     // Start is called before the first frame update
     void Start()
