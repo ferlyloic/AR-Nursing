8423b39cde6bacab01f301404b8f13a06941512c
ferlyloic <ferlyloic@yahoo.fr>
2020-05-26 11:34:19 +0200

    ImageTracking Skript added

---
+++ Assets/Scripts/ImageTracking.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+
+public class ImageTracking : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] prefabs;
+
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+    private ARTrackedImageManager trackedImageManager;
+
+    private void Awake()
+    {
+        trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+        foreach (GameObject prefab in prefabs)
+        {
+            GameObject newPrefab = Instantiate(prefab,Vector3.zero,Quaternion.identity);
+            newPrefab.name = prefab.name;
+            spawnedPrefabs.Add(newPrefab.name, newPrefab);
+        }
+    }
+
+    private void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += ImageChanged;
+    }
+
+    private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach(ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        {
+            spawnedPrefabs[trackedImage.name].SetActive(false);
+        }
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage)
+    {
+        string name = trackedImage.referenceImage.name;
+        Vector3 position = trackedImage.transform.position;
+        GameObject prefab = spawnedPrefabs[name];
+        prefab.transform.position = position;
+        prefab.SetActive(true);
+
+        foreach(GameObject gameObject in spawnedPrefabs.Values)
+        {
+            if(gameObject.name != name)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
